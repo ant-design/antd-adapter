9810015af9a144c608399cdae1df1e062cfb4875
Benjy Cui <benjytrys@gmail.com>
2016-12-23 12:06:43 +0800

    feat: add DatePickerAdapter

--- 1to2/__tests__/DatePicker.test.jsx
+++ 1to2/__tests__/DatePicker.test.jsx
@@ -1,5 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moment from 'moment';
+import DatePicker from 'antd/lib/date-picker';
+import DatePickerAdapter from '../DatePicker';
+import Picker from 'rc-calendar/lib/Picker';
+
 describe('DatePicker', () => {
-  it('should be OK', () => {
-    expect(true).toBe(true);
+  it('should convert old format to new format', () => {
+    const format = mount(<DatePickerAdapter format="yyyy-MM-dd HH:mm:ss" />)
+            .find(DatePicker).prop('format');
+    expect(format).toBe('YYYY-MM-DD HH:mm:ss');
+  });
+
+  it('should support string as value/defaultValue', () => {
+    const value = mount(<DatePickerAdapter value="2016-12-23" />)
+            .find(DatePicker).prop('value');
+    expect(value.isSame('2016-12-23', 'day')).toBe(true);
+
+    const defaultValue = mount(<DatePickerAdapter defaultValue="2016-12-23" />)
+            .find(DatePicker).prop('defaultValue');
+    expect(defaultValue.isSame('2016-12-23', 'day')).toBe(true);
+  });
+
+  it('should support Date as value/defaultValue', () => {
+    const value = mount(<DatePickerAdapter value={new Date()} />)
+            .find(DatePicker).prop('value');
+    expect(value.isSame(new Date(), 'day')).toBe(true);
+
+    const defaultValue = mount(<DatePickerAdapter defaultValue={new Date()} />)
+            .find(DatePicker).prop('defaultValue');
+    expect(defaultValue.isSame(new Date(), 'day')).toBe(true);
+  });
+
+  it('should convert args in disabledDate to GregorianCalendar', () => {
+    expect(() => {
+      mount(
+        <DatePickerAdapter
+          disabledDate={current => current && current.getTime() > Date.now()}
+        />
+      ).find(Picker).find('span')
+        .first().simulate('click');
+    }).not.toThrow();
+  });
+
+  it('should convert args in onChange to GregorianCalendar', () => {
+    expect(() => {
+      mount(<DatePickerAdapter onChange={value => console.log(value.getTime())} />)
+        .find(DatePicker).prop('onChange')(moment());
+    }).not.toThrow();
   });
 });
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # antd-adapter
+
+An adapter which makes newer antd works like old one. DIRTY HACK \\ T^T /
+
+## Installation
+
+```bash
+npm i --save antd-adapter
+```
+
+## Usage
+
+````jsx
+import DatePicker from 'antd-adapter/1to2/DatePicker';
+
+// Then, you can use DatePicker's APIs as old antd
+<DatePicker value="2016-12-23" />
+````
+
+## Supported Adapter
+
+````jsx
+import DatePicker from 'antd-adapter/1to2/DatePicker';
+````
+
+## License
+
+MIT
